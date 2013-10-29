1aef1ea72c3074f3e61063cb90bac4035668efad
rakhmadikhsanudin <rakhmadikhsanudin@4d172998-2a9d-66a3-de1c-bc76e3955fda>
2013-10-29 15:17:18 +0000

    slide udah jadi

--- trunk/src/id/artefact/kiblat/MainActivity.java
+++ trunk/src/id/artefact/kiblat/MainActivity.java
@@ -1,15 +1,28 @@
 package id.artefact.kiblat;
 
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
 
+	private SlidingMenu slidingMenu ;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		SlidingMenu menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setShadowWidthRes(R.dimen.shadow_width);
+        menu.setShadowDrawable(R.drawable.shadow);
+        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        menu.setFadeDegree(0.35f);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        menu.setMenu(R.layout.activity_splash);
 	}
 
 	@Override

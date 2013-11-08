7277dc801bcce855bd6e6e36f9ba725aae381e68
rakhmadikhsanudin <rakhmadikhsanudin@4d172998-2a9d-66a3-de1c-bc76e3955fda>
2013-11-08 16:37:39 +0000

    header listview

--- src/id/artefact/kiblat/AboveFragment.java
+++ src/id/artefact/kiblat/AboveFragment.java
@@ -1,25 +1,43 @@
 package id.artefact.kiblat;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.markupartist.android.widget.PullToRefreshListView;
 import com.markupartist.android.widget.PullToRefreshListView.OnRefreshListener;
 
+
 import id.artefact.kiblat.help.LazyAdapterAbove;
 import id.artefact.kiblat.help.LazyAdapterBehindMenu;
 import id.artefact.kiblat.help.ServiceHelper;
+import android.annotation.SuppressLint;
+import android.app.ActionBar.LayoutParams;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -36,6 +54,7 @@ public class AboveFragment extends ListFragment {
 		return inflater.inflate(R.layout.list_above, null);
 	}
 
+	@SuppressLint("NewApi")
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 		// SampleAdapter adapter = new SampleAdapter(getActivity());
@@ -71,7 +90,8 @@ public class AboveFragment extends ListFragment {
 		}
 		adapter = new LazyAdapterAbove(getActivity(), abovelist);
 		setListAdapter(adapter);
-
+		View header = getActivity().getLayoutInflater().inflate(R.layout.headerlist, null);
+		getListView().addHeaderView(header);
 	}
 
 	private class SampleItem {
@@ -119,25 +139,30 @@ public class AboveFragment extends ListFragment {
 		private ProgressDialog dialog = new ProgressDialog(getActivity());
 
 		protected void onPreExecute() {
-			dialog.setMessage("Loading....");
-			dialog.show();
+			// dialog.setMessage("Loading....");
+			// dialog.show();
 		}
 
 		@Override
 		protected void onPostExecute(Boolean result) {
 			// TODO Auto-generated method stub
-			dialog.dismiss();
+			((PullToRefreshListView) getListView()).onRefreshComplete();
+			// dialog.dismiss();
 		}
 
 		@Override
 		protected Boolean doInBackground(String... params) {
 			// TODO Auto-generated method stub
-			ServiceHelper srv = new ServiceHelper();
-			if (srv.getPost(getActivity()) != null) {
+			try {
+				ServiceHelper srv = new ServiceHelper();
+				String srvberitaterkini = srv.tes();
+				Log.i("xmlrpc", srvberitaterkini);
 				return true;
-			} else {
+			} catch (Exception e) {
+				// TODO: handle exception
 				return false;
 			}
 		}
 	}
+	
 }
